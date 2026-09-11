fdfdefb2cfb673b0c8bad9abbaf7b75cfdd23995
DataMasr <MadelineBrining36@hotmail.com>
2026-09-11 03:07:50 +0300

    Delete scratch directory

--- scratch/print-nav.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf8');
-const navStart = html.indexOf('<header class="main-nav-wrapper">');
-const navEnd = html.indexOf('</header>');
-console.log(html.substring(navStart, navEnd + 9));
